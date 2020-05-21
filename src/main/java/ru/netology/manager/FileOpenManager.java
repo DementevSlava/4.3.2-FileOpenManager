6549bf72aab8120ae072917229e3764a1d8ced9f
Dementev Slava <kursant344@gmail.com>
2020-05-21 09:38:42 +0300

    add FileOpenManagerTest.java

---
+++ src/main/java/ru/netology/manager/FileOpenManager.java
@@ -0,0 +1,50 @@
+package ru.netology.manager;
+
+import java.util.*;
+
+public class FileOpenManager {
+
+    private HashMap<String, String> fileManager = new HashMap<>();
+
+    public void addExtension(String extension) {
+        fileManager.put(extension, null);
+    }
+
+    public HashMap<String, String> getAllBinding() {
+        return fileManager;
+    }
+
+    public void addProgramName(String extension, String programName) {
+        fileManager.replace(extension, programName);
+    }
+
+    public String findProgByExt(String extension) {
+        return fileManager.get(extension);
+    }
+
+    public void delBinding(String extension) {
+        if (fileManager.containsKey(extension)) {
+            fileManager.replace(extension, null);
+        }
+    }
+
+    public List findAllExt(HashMap<String, String> list) {
+        List<String> keys = new ArrayList<>();
+        for (Map.Entry entry : list.entrySet())
+            if (entry.getValue() != null) {
+                keys.add((String) entry.getKey());
+            }
+        Collections.sort(keys);
+        return keys;
+    }
+
+   public List findAllProg(HashMap<String, String> list) {
+        List<String> program = new ArrayList<>();
+        for (String prog : list.values())
+            if ( prog != null) {
+                program.add(prog);
+            }
+        Collections.sort(program);
+            return program;
+    }
+}
---
+++ src/test/java/ru/netology/manager/FileOpenManagerTest.java
@@ -0,0 +1,62 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FileOpenManagerTest {
+    FileOpenManager manager = new FileOpenManager();
+    String html = "html";
+    String pdf = "pdf";
+    String avi = "avi";
+    String jpg = "jpg";
+    String png = "png";
+    String fireFox = "FireFox";
+    String adobeReader = "AdobeReader";
+    String vlc = "VLC";
+    String paint = "Paint";
+
+    @BeforeEach
+    void setUp() {
+        manager.addExtension(html);
+        manager.addExtension(pdf);
+        manager.addExtension(avi);
+        manager.addExtension(jpg);
+        manager.addExtension(png);
+        manager.addProgramName(html, fireFox);
+        manager.addProgramName(avi, vlc);
+    }
+
+    @Test
+    void findProgByExt() {
+        String actual = manager.findProgByExt(avi);
+        String expected = vlc;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void delBinding() {
+        manager.delBinding(html);
+        System.out.println(manager.getAllBinding());
+    }
+
+    @Test
+    void findAllExt() {
+        List<String> actual = manager.findAllExt(manager.getAllBinding());
+        List<String> expected = Arrays.asList(avi, html);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAllProg() {
+        List<String> actual = manager.findAllProg(manager.getAllBinding());
+        List<String> expected = Arrays.asList(fireFox, vlc);
+
+        assertEquals(expected, actual);
+    }
+}
